6189ec25c763e09d234b495581a6896f2c7886e2
Temnikova Valeriia <valtty143@gmail.com>
2026-06-05 14:07:26 +0900

    4

--- lash_salon/settings.py
+++ lash_salon/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
